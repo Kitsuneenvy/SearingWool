3fc9b5e9099a4e8e1fd76ba1a381142fbab7f851
KitsuneEnvy <lachlangodthelp@hotmail.com>
2016-08-20 13:54:44 +1000

    Added gui

--- Worlds/Assets/C# Scripts/Manager.cs
+++ Worlds/Assets/C# Scripts/Manager.cs
@@ -10,10 +10,26 @@ public class Manager : MonoBehaviour {
 	public Texture rotateIcon;
 	public int currentLayer = 1;
 	string debugString = "";
+	public Texture2D counterBackground = null;
+	public Texture2D pauseButton = null;
+	Rect counterRect = new Rect(0,0,0,0);
+	Rect pauseRect = new Rect(0,0,0,0);
+	Rect menuRect = new Rect(0,0,0,0);
+	bool menuOpen = false;
+	public GUIStyle counterFont;
 
 
 	// Use this for initialization
 	void Start () {
+		counterRect.width = counterBackground.width;
+		counterRect.height = counterBackground.height;
+		pauseRect.x = counterBackground.width;
+		pauseRect.width = pauseButton.width;
+		pauseRect.height = pauseButton.height;
+		menuRect.x = Screen.width/3;
+		menuRect.y = Screen.height/5;
+		menuRect.width = Screen.width/3;
+		menuRect.height = Screen.height/3;
 		worldSettingsObject = GameObject.FindGameObjectWithTag("WorldObject");
 	}
 	
@@ -22,32 +38,32 @@ public class Manager : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUILayout.BeginHorizontal();
-		if(rotateCamera==true){
-			if(GUILayout.Button(new GUIContent(rotateIcon))){
-				rotateCamera = !rotateCamera;
+		if(!menuOpen){
+			GUI.Label(counterRect,counterBackground,counterFont);
+			GUI.Label(counterRect, worldSettingsObject.GetComponent<WorldSettings>().returnTurns().ToString(),counterFont);
+			if(GUI.Button(pauseRect,pauseButton,counterFont)){
+				menuOpen=true;
 			}
 		} else {
-			if(GUILayout.Button(new GUIContent(cameraIcon))){
-				rotateCamera = !rotateCamera;
-			}
+			GUI.Window(0,menuRect,menuFunction,"Menu");
 		}
-		if(GUILayout.Button("<-")){
-			if(currentLayer>1){
-				currentLayer--;
-			}
-		}
-		GUILayout.Label(currentLayer.ToString());
-		GUILayout.Label(debugString.ToString());
-		if(GUILayout.Button("->")){
-			if(currentLayer<worldSettingsObject.GetComponent<WorldSettings>().Layers.Count){
-				currentLayer++;
-			}
-		}
-		GUILayout.Label("Remaining turns : " + worldSettingsObject.GetComponent<WorldSettings>().returnTurns().ToString());
-		GUILayout.EndHorizontal();
+//		GUILayout.BeginHorizontal();
+//		GUILayout.Label(counterBackground);
+//		GUILayout.Label("Remaining turns : " + worldSettingsObject.GetComponent<WorldSettings>().returnTurns().ToString());
+//		GUILayout.EndHorizontal();
 	}
 
+	void menuFunction(int id){
+		GUILayout.BeginVertical();
+		if(GUILayout.Button("Unpause")){
+			menuOpen = false;
+		}
+		if(GUILayout.Button("Quit")){
+			Application.Quit();
+		}
+		GUILayout.EndVertical();
+
+	}
 	public int returnCurrentLayer(){
 		return currentLayer;
 	}
